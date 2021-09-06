6fe1750410f8d2362a3db81c85e43afc6dd96ee3
aokinorifumi <n.aoki.time.limit.tenyears@gmail.com>
2021-09-06 19:08:21 +0900

    変更1

--- db/migrate/20210824080047_create_posts.rb
+++ db/migrate/20210824080047_create_posts.rb
@@ -1,7 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.text :content
+      t.integer :format_id, null: false
+      t.text :content, null: false
 
       t.timestamps
     end
