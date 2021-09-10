b1dee82724012767775e25311441958ce99da03c
aokinorifumi <n.aoki.time.limit.tenyears@gmail.com>
2021-09-11 04:49:55 +0900

    todoの削除ボタン実装まで

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,28 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.order(id: "DESC")
+    posts = Post.order(id: "DESC")
+    @post1 = posts.where(format: "1")
+    @post2 = posts.where(format: "2")
   end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index  # 追記する
+    @post = Post.new(post_params)
+    if @post.valid?
+       @post.save
+       redirect_to posts_path
+    end
   end
 
+  def destroy
+     @post = Post.find(params[:id])
+     @post.destroy
+     redirect_to posts_path
+  end 
+
+  private
+
+  def post_params
+    params.permit(:format, :content)
+  end
 end
--- db/migrate/20210824080047_create_posts.rb
+++ db/migrate/20210824080047_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.integer :format_id, null: false
+      t.integer :format, null: false 
       t.text :content, null: false
 
       t.timestamps
